9ddb916324964f3142128689dee940ad361e0e35
foxy82 <neil.renaud@gmail.com>
2017-04-25 09:42:05 +0100

    Create queue.py

---
+++ alexaaudio/queue.py
@@ -0,0 +1,16 @@
+class AlexaAudioQueue(object):
+
+    def __init__(self, urls):
+        self._urls = urls
+        
+    def status(self):
+        pass()
+
+    def up_next(self):
+        pass()
+
+    def shuffle(self):
+        pass()
+        
+    def loop(self):
+        pass()
